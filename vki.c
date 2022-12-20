b8b887acb26a7350f21b444bef7168a62ff40c7e
Yunus Emre Çakmak <115035453+cakmakkk@users.noreply.github.com>
2022-12-20 23:10:10 +0300

    Add files via upload

---
+++ vki.c
@@ -0,0 +1,20 @@
+#include <stdio.h> //hocam imzaladi diye githuba atmamistim.
+int main(){
+     double boy,kilo,ind;
+
+     printf("Kilonuz : "); scanf("%lf",&kilo);
+     printf("Metre Cinsinden Boyunuz : "); scanf("%lf",&boy);
+     ind=kilo/(boy*boy);
+
+     printf("Vucut kitle indeksiniz : %f \n", ind);
+     if(ind<18)
+          printf("Zayif");
+     else if(ind>=18 && ind<25)
+          printf("Normal");
+     else if(ind>=25 && ind<30)
+          printf("Kilolu");
+     else if(ind>=30 && ind<40)
+          printf("Obez");
+     else
+          printf("Ileri Derece Obez");
+}
